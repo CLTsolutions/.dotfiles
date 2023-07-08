88ff0ec0f17080a1314bb43906e41b10b97395af
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-07-08 16:58:28 -0400

    chore(zsh): rm file aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,13 +9,6 @@ alias as='code ~/.config/zsh/aliases.zsh'
 alias dotfiles='~/.dotfiles'
 alias nvconfig='nvim ~/.config/nvim/init.vim'
 
-# files
-alias jonas='/Users/batheart/workspace/udemy/advanced-css-course'
-alias port='/Users/batheart/workspace/portfolio/react_portfolio'
-alias rubyist='/Users/batheart/workspace/the_rubyist'
-alias tweets='/Users/batheart/workspace/rails_for_beginners/scheduled_tweets'
-alias wk='cd /Users/batheart/workspace'
-
 # git
 alias gco='git checkout'
 alias gcb='git checkout -b'
