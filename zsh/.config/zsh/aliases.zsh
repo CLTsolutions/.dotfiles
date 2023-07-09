de7f4ea57b16e78d81abfff2c1bc5c7381131c8e
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-07-09 14:41:15 -0400

    fix(zsh): path the alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,10 +3,11 @@ alias ..='cd ..'
 alias ~='cd ~'
 alias c='clear'
 alias opf='open -a finder $(pwd)'
+alias rszsh='source ~/.zshrc'
 
 # configs
 alias as='code ~/.config/zsh/aliases.zsh'
-alias dotfiles='~/.dotfiles'
+alias dotfiles='cd ~/.dotfiles'
 alias nvconfig='nvim ~/.config/nvim/init.vim'
 
 # git
