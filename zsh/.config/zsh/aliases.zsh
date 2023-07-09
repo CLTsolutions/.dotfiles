8c9c492abafeba12dcbbdd0d9716d97b9e64ae19
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-07-09 15:07:19 -0400

    chore(zsh): rm git aliases and use zap plugin

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,15 +3,11 @@ alias ..='cd ..'
 alias ~='cd ~'
 alias c='clear'
 alias opf='open -a finder $(pwd)'
-alias rszsh='source ~/.zshrc'
+alias zshrcs='source ~/.zshrc'
+alias zshrc='code ~/.zshrc'
 
 # configs
 alias as='code ~/.config/zsh/aliases.zsh'
 alias dotfiles='cd ~/.dotfiles'
-alias nvconfig='nvim ~/.config/nvim/init.vim'
-
-# git
-alias gco='git checkout'
-alias gcb='git checkout -b'
-alias gcm='git checkout master'
-alias gst='git status'
+alias nvimrc='nvim ~/.config/nvim/init.vim'
+alias tmuxrc='nvim ~/.tmux.conf'
