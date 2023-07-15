39c212860389f96b97a04d7c74bacfebbe75f7e8
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-07-15 12:59:15 -0400

    chore(zsh): lazygit alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -12,3 +12,6 @@ alias dotfiles='cd ~/.dotfiles'
 alias kittyrc='code ~/.config/kitty/kitty.conf'
 alias nvimrc='nvim ~/.config/nvim/init.vim'
 alias tmuxrc='nvim ~/.tmux.conf'
+
+# commands
+alias lg='lazygit'
